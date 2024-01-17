2b33f3ce0acfaa97dd0b769921f79a35f1d5f8c6
João Paulo <103288654+Jounys@users.noreply.github.com>
2024-01-16 21:53:48 -0300

    Melhorias geral, add nova função de reinicio, css em andameto de melhoria

--- 08_Forca/script.js
+++ 08_Forca/script.js
@@ -1,64 +1,83 @@
 // Verificar se a letra ja foi enviada e se for verdade retornar -(menos) uma vida
 // Add as letras enviadas em um array para comparação futura
 // Verificar se a letra está correta e imprimir na tela
-let letras = []
-let vida = 5
-let palavraAleatoria = ['arroz', 'carne', 'feijao', 'maria', 'tapioca', 'cafe']
+let palavraAleatoria = ['arroz', 'carne', 'feijao', 'maria', 'tapioca', 'cafe', 'macarrao', 'munguza', 'frango', 'joao', 'fernando', 'rita', 'rebeca', 'josefa', 'wilson', 'ilza', 'rian', 'zara', 'suzi', 'luva', 'cadeira', 'telivisao']
 
+const res = document.getElementById('res')
 const numAleatorio = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
 const pegarPalavra = () => palavraAleatoria[numAleatorio(0, palavraAleatoria.length - 1)]
+let vida = 6
+let letras = []
 let forca = pegarPalavra()
 
+document.getElementById('dicaInicial').innerHTML = `Dica: Tem ${forca.length} letras.`
 document.getElementById('botaoEnviar').addEventListener('click', jogar)
 
 function jogar(){
-    const txt = document.getElementById('txt').value
-    const res = document.getElementById('res')
-    const vidaMostrar = document.getElementById('vida')
-    const letrasForca = document.getElementById('letras')
+    const txt = document.getElementById('txt').value.trim().toLowerCase();
+    const mostrarVida = document.getElementById('vida');
+    const letrasForca = document.getElementById('letras');
+
+    if(txt === ''){
+        alert('Digite uma letra.')
+    }
 
     // Add as letras do user no array e se não for true perde uma vida e verifica se a letra ja foi enviado perdendo uma vida tbm
     if(letras.includes(txt)){
-        alert('letras ja enviada')
+        alert('letras já enviada.')
         vida--
-        vidaMostrar.innerHTML = `Vidas: ${vida}`
     } else {
         letras.push(txt)
         if(!forca.includes(txt)){
-            alert('Errado! Perdeu uma vida')
+            alert('Errado! Perdeu uma vida.')
             vida--
-            vidaMostrar.innerHTML = `Vidas: ${vida}`
         }
         letrasForca.innerHTML += `${txt} `
     }
 
     if(vida <= 0){
         alert('Fim de jogo. Você perdeu!')
+        reiniciarJogo();
     }
 
-    let array = forca.split('')
-    let letrasAdivinhadas = '';
-
-    if(txt.trim() === ''){
-        alert('Digite uma letra.')
-    } else {
-        for(let i = 0; i < array.length; i++){
-            if(letras.includes(array[i])){ 
-                letrasAdivinhadas += `${array[i]} `    
-            } else {
-                letrasAdivinhadas += '_ ';
-            }
-        }
-        res.innerHTML = letrasAdivinhadas;
-        console.log(letrasAdivinhadas)
-    }
+    atualizarPalavra()
+    mostrarVida.innerHTML = `Vidas: ${vida}`;
 
     if (forca.split('').every(letra => letras.includes(letra))) {
         alert('Parabéns! Você acertou a palavra!');
         console.log(forca.split(''))
+        reiniciarJogo();
         // Aqui você pode fazer o que desejar ao acertar a palavra, por exemplo, reiniciar o jogo.
     }
+
+    document.getElementById('txt').value = '';
+    document.getElementById('txt').focus();
     
     console.log(letras)
     console.log(vida)
+}
+
+function atualizarPalavra(){
+    let letrasAdivinhadas = '';
+
+    for(let i = 0; i < forca.length; i++){
+        if(letras.includes(forca[i])){
+            letrasAdivinhadas += `${forca[i]} `
+        } else {
+            letrasAdivinhadas += '_ '
+        }
+    }
+    res.innerHTML = letrasAdivinhadas
+}
+
+function reiniciarJogo(){
+    letras = [];
+    vida = 8;
+    forca = pegarPalavra();
+    location.reload(true);
+
+    document.getElementById('txt').value = '';
+    document.getElementById('res').innerHTML = '';
+    document.getElementById('letras').innerHTML = '';
+    document.getElementById('vida').innerHTML = `Vidas: ${vida}`;
 }
